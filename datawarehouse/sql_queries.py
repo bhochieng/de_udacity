aedff081e5e2e4f62a84c8207c9e1492fa93ccf6
HumphreyOchieng <developer.humphrey@gmail.com>
2022-12-19 13:39:09 +0300

    sql script cleaning

--- datawarehouse/sql_queries.py
+++ datawarehouse/sql_queries.py
@@ -4,25 +4,14 @@ import configparser
 # CONFIG
 config = configparser.ConfigParser()
 config.read('/Users/bhochieng/Documents/projects/de_udacity/datawarehouse/dwh.cfg')
-files = config.read('/Users/bhochieng/Documents/projects/de_udacity/datawarehouse/dwh.cfg')
-print(files)
 
 
 LOG_DATA = config.get("S3", "LOG_DATA")
 LOG_JSONPATH = config.get("S3", "LOG_JSONPATH")
 SONG_DATA = config.get("S3", "SONG_DATA")
 ARN = config.get("IAM_ROLE", "ARN")
-#REGION = config.get('GEO', 'REGION')
 
 # DROP TABLES
-
-# staging_events_table_drop = ""
-# staging_songs_table_drop = ""
-# songplay_table_drop = ""
-# user_table_drop = ""
-# song_table_drop = ""
-# artist_table_drop = ""
-# time_table_drop = ""
 
 
 staging_events_table_drop = "DROP table IF EXISTS staging_events;"
@@ -35,26 +24,6 @@ time_table_drop = "DROP table IF EXISTS time;"
 
 # CREATE TABLES
 
-# staging_events_table_create= ("""
-# """)
-
-# staging_songs_table_create = ("""
-# """)
-
-# songplay_table_create = ("""
-# """)
-
-# user_table_create = ("""
-# """)
-
-# song_table_create = ("""
-# """)
-
-# artist_table_create = ("""
-# """)
-
-# time_table_create = ("""
-# """)
 staging_events_table_create= ("""
 CREATE TABLE staging_songs
 (
@@ -158,11 +127,6 @@ time_table_create = ("""
 
 # STAGING TABLES
 
-# staging_events_copy = ("""
-# """).format()
-
-# staging_songs_copy = ("""
-# """).format()
 staging_events_copy = ("""
     COPY staging_events 
         FROM {} 
@@ -184,20 +148,6 @@ staging_songs_copy = ("""
 
 # FINAL TABLES
 
-# songplay_table_insert = ("""
-# """)
-
-# user_table_insert = ("""
-# """)
-
-# song_table_insert = ("""
-# """)
-
-# artist_table_insert = ("""
-# """)
-
-# time_table_insert = ("""
-# """)
 songplay_table_insert = ("""
 
     INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
@@ -269,12 +219,6 @@ time_table_insert = ("""
 """)
 
 # QUERY LISTS
-
-# create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-# drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-# copy_table_queries = [staging_events_copy, staging_songs_copy]
-# insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
-
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create,song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplays_table_drop, users_table_drop,songs_table_drop, artists_table_drop, time_table_drop]
